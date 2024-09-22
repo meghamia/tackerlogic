1483be22e0122c67eed418d91ee6b2938fbbdf3e
megha <87981400+meghamia@users.noreply.github.com>
2024-09-22 20:16:40 +0530

    Update dbhelper.dart

--- lib/database/dbhelper.dart
+++ lib/database/dbhelper.dart
@@ -1,26 +1,33 @@
+
+import 'package:get/get.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
 import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   // Table names
   static const String tableTasks = 'tasks';
-  static const String tableTaskUpdates = 'task_updates';
+  static const String tableTaskTrack = 'task_tracked';
 
   // Column names for tasks table
   static const String columnId = 'id';
   static const String columnTask = 'task';
-  static const String columnIsChecked = 'is_checked';
-  static const String columnProgress = 'progress';
-  static const String columnCreatedDate = 'created_date';
-  static const String columnUpdatedDate = 'updated_date';
-  static const String columnDayName = 'day_name'; // New column for day name
+  static const String columnIsTracked = 'is_tracked';
+  //static const String columnProgress = 'progress';
 
-  // Column names for task_updates table
-  static const String columnUpdateId = 'id'; // Assuming same ID column
-  static const String columnTaskName = 'task_name';
-  static const String columnCompleted = 'completed';
-  static const String columnUpdateProgress = 'progress';
-  static const String columnUpdateDate = 'updated_date';
+  //static const String columnUpdatedDate = 'created_date';
+
+  // Column names for task_tracked table
+  static const String columnUpdateId = 'id';
+  static const String columnTaskId = 'task_id';
+ // static const String columnCompleted = 'completed';
+ // static const String columnUpdateProgress = 'progress';
+  static const String columnTrackedDate = 'tracked_date';
 
   // Singleton pattern
   static final DatabaseHelper _instance = DatabaseHelper._internal();
@@ -41,80 +48,129 @@ class DatabaseHelper {
       'tasks.db',
       version: 2, // Increment the version number if there are schema changes
       onCreate: (db, version) async {
-        await db.execute(''' 
+        await db.execute('''
           CREATE TABLE $tableTasks (
             $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
-            $columnTask TEXT,
-            $columnIsChecked INTEGER,
-            $columnProgress REAL,
-            $columnCreatedDate TEXT,
-            $columnUpdatedDate TEXT,
-            $columnDayName TEXT
+            $columnTask TEXT
           )
         ''');
         await db.execute('''
-          CREATE TABLE $tableTaskUpdates (
+          CREATE TABLE $tableTaskTrack (
             $columnUpdateId INTEGER PRIMARY KEY AUTOINCREMENT,
-            $columnTaskName TEXT,
-            $columnCompleted INTEGER,
-            $columnUpdateProgress REAL,
-            $columnUpdateDate TEXT
+            $columnTaskId INTEGER,
+            $columnTrackedDate TEXT
           )
         ''');
       },
-      onUpgrade: (db, oldVersion, newVersion) async {
-        if (oldVersion < 2) { // Upgrade to version 2
-          // Add the new column to the existing table
-          await db.execute(''' 
-            ALTER TABLE $tableTasks ADD COLUMN $columnDayName TEXT
-          ''');
-        }
-        // You can add further upgrade logic here for future versions
-      },
+      // onUpgrade: (db, oldVersion, newVersion) async {
+      //   if (oldVersion < 2) {
+      //     // Upgrade to version 2
+      //     // Add the new column to the existing table
+      //     await db.execute('''
+      //       ALTER TABLE $tableTasks ADD COLUMN $columnDayName TEXT
+      //     ''');
+      //   }
+      //   // You can add further upgrade logic here for future versions
+      // },
     );
   }
 
   // Method to insert a new task
-  Future<int> insertTask(String task, bool isChecked, double progress, String createdDate, String updatedDate) async {
+  Future<int> insertTask(String task) async {
     final db = await database;
     return await db.insert(
       tableTasks,
       {
         columnTask: task,
-        columnIsChecked: isChecked ? 1 : 0,
-        columnProgress: progress,
-        columnCreatedDate: createdDate,
-        columnUpdatedDate: updatedDate,
-        columnDayName: DateFormat('EEEE').format(DateTime.now()), // Add day name
+        // columnTrackedDate :  DateFormat('DD-MM-YYYY').format(DateTime.now()), // Add day name
+
+        // columnIsChecked: isChecked ? 1 : 0,
+        // columnProgress: progress,
+        // columnTrackedDate:  DateFormat('DD-MM-YYYY').format(DateTime.now()), // Add day name
+
+        // columnDayName:
+        // DateFormat('EEEE').format(DateTime.now()), // Add day name
       },
     );
+  }
+
+
+
+
+  // Method to insert a new task update
+  Future<int> insertTrackTask(int id) async {
+    final db = await database;
+    return await db.insert(
+      tableTaskTrack,
+      {
+        columnTaskId: id,
+        columnTrackedDate :  DateFormat('dd-MM-yyyy').format(DateTime.now()),
+
+
+        //columnCompleted: isChecked ? 1 : 0,
+        //columnUpdateProgress: progress,
+        //columnUpdateDate: updatedDate,
+      },
+
+    );
+
+
   }
 
   // Method to update an existing task
-  Future<int> updateTask(String oldTask, String newTask, bool isChecked, double progress, String updatedDate) async {
+  // Future<int> updateTask(String oldTask, String newTask, bool isChecked,
+  //     double progress, String updatedDate) async {
+  //   final db = await database;
+  //   return await db.update(
+  //     tableTasks,
+  //     {
+  //       columnTask: newTask,
+  //       columnIsChecked: isChecked ? 1 : 0,
+  //       columnProgress: progress,
+  //       columnUpdatedDate: updatedDate,
+  //     },
+  //     where: '$columnTask = ?',
+  //     whereArgs: [oldTask],
+  //   );
+  // }
+
+  // Method to delete a task
+  Future<void> deleteTrackTask(int taskId,String trackedDate) async {
     final db = await database;
-    return await db.update(
-      tableTasks,
-      {
-        columnTask: newTask,
-        columnIsChecked: isChecked ? 1 : 0,
-        columnProgress: progress,
-        columnUpdatedDate: updatedDate,
-      },
-      where: '$columnTask = ?',
-      whereArgs: [oldTask],
+
+    // await db.delete(
+    //   tableTasks,
+    //   where: '$columnId = ?',
+    //   whereArgs: [taskId],
+    // );
+
+
+    await db.delete(
+      tableTaskTrack,
+      where: '$columnTaskId = ? AND $columnTrackedDate = ?',
+      whereArgs: [taskId,trackedDate],
     );
   }
 
-  // Method to delete a task
-  Future<int> deleteTask(String task) async {
+  Future<void> deleteTask(int taskId) async {
     final db = await database;
-    return await db.delete(
+    await db.delete(
       tableTasks,
-      where: '$columnTask = ?',
-      whereArgs: [task],
+      where: '$columnId = ?',
+      whereArgs : [taskId],
     );
   }
+
+  Future<bool> isTaskTrackedForDate(int taskId, String trackedDate) async {
+    final db = await database;
+    final result = await db.query(
+      tableTaskTrack,
+      where: '$columnTaskId = ? AND $columnTrackedDate = ?',
+      whereArgs: [taskId, trackedDate],
+    );
+    return result.isNotEmpty;
+  }
+
 
   // Method to fetch all tasks
   Future<List<Map<String, dynamic>>> getTasks() async {
@@ -122,65 +178,68 @@ class DatabaseHelper {
     return await db.query(tableTasks);
   }
 
-  // Method to insert a new task update
-  Future<int> insertTaskUpdate(String taskName, bool isChecked, double progress, String updatedDate) async {
-    final db = await database;
-    return await db.insert(
-      tableTaskUpdates,
-      {
-        columnTaskName: taskName,
-        columnCompleted: isChecked ? 1 : 0,
-        columnUpdateProgress: progress,
-        columnUpdateDate: updatedDate,
-      },
-    );
-  }
+  // Future<List<Map<String,dynamic>>> getTrackTask(String date,int id) async{
+  //   final db = await database;
+  //   // return await db.query(tableTasks);
+  //   return await db.query(
+  //           tableTaskTrack,
+  //           where: '$columnTaskId = ? AND $columnTrackedDate = ?',
+  //           whereArgs: [id,date],
+  //         );
+  //
+  // }
 
 
 
-  // Method to fetch task data for a specific week
-  Future<List<Map<String, dynamic>>> fetchTaskDataForWeek(String taskName) async {
-    final db = await database;
-    String endDate = DateTime.now().toIso8601String();
-    String startDate = DateTime.now().subtract(Duration(days: 7)).toIso8601String();
-
-    return await db.query(
-      tableTaskUpdates,
-      where: '$columnTaskName = ? AND $columnUpdateDate BETWEEN ? AND ?',
-      whereArgs: [taskName, startDate, endDate],
-    );
-  }
+  // // Method to fetch task data for a specific week
+  // Future<List<Map<String, dynamic>>> fetchTaskDataForWeek(
+  //     String taskName) async {
+  //   final db = await database;
+  //   String endDate = DateTime.now().toIso8601String();
+  //   String startDate =
+  //       DateTime.now().subtract(Duration(days: 7)).toIso8601String();
+  //
+  //   return await db.query(
+  //     tableTaskUpdates,
+  //     where: '$columnTaskName = ? AND $columnUpdateDate BETWEEN ? AND ?',
+  //     whereArgs: [taskName, startDate, endDate],
+  //   );
+  // }
 
   // Method to get task history
-  Future<List<Map<String, dynamic>>> getTaskHistory(String task) async {
-    final db = await database;
-    return await db.query(
-      tableTaskUpdates,
-      where: '$columnTaskName = ?',
-      whereArgs: [task],
-    );
-  }
+  // Future<List<Map<String, dynamic>>> getTaskHistory(String task) async {
+  //   final db = await database;
+  //   return await db.query(
+  //     tableTaskUpdates,
+  //     where: '$columnTaskName = ?',
+  //     whereArgs: [task],
+  //   );
+  // }
 
-  // Method to update the day name for a task
-  Future<int> updateTaskDayName(String taskName, String dayName) async {
-    final db = await database;
-    return await db.update(
-      tableTasks,
-      {
-        columnDayName: dayName,
-      },
-      where: '$columnTask = ?',
-      whereArgs: [taskName],
-    );
-  }
+  // // Method to update the day name for a task
+  // Future<int> updateTaskDayName(String taskName, String dayName) async {
+  //   final db = await database;
+  //   return await db.update(
+  //     tableTasks,
+  //     {
+  //       columnDayName: dayName,
+  //     },
+  //     where: '$columnTask = ?',
+  //     whereArgs: [taskName],
+  //   );
+  // }
 
   // Method to fetch task details by task name
-  Future<List<Map<String, dynamic>>> getTaskDetails(String taskName) async {
-    final db = await database;
-    return await db.query(
-      tableTasks,
-      where: '$columnTask = ?',
-      whereArgs: [taskName],
-    );
-  }
+  // Future<List<Map<String, dynamic>>> getTaskDetails(String taskName) async {
+  //   final db = await database;
+  //   return await db.query(
+  //     tableTasks,
+  //     where: '$columnTask = ?',
+  //     whereArgs: [taskName],
+  //   );
+  // }
+
+
+
+
 }
