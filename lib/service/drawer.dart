dd480236f6eecf3ba19ea65a0a030b4f3ad0527f
megha <87981400+meghamia@users.noreply.github.com>
2024-10-30 17:42:55 +0530

    Update drawer.dart

--- lib/service/drawer.dart
+++ lib/service/drawer.dart
@@ -10,20 +10,17 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       child: Stack(
         children: [
-          // Background image for the drawer
           Image.asset(
             'assets/images/img_2.png',
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
           ),
-          // Drawer background color based on theme
           Container(
             color: Theme.of(context).brightness == Brightness.light
                 ? Color(0xFFF5F5FA) // Light theme background color
                 : Color(0xFF3E3E3E), // Dark theme background color
           ),
-          // Content on top of the image and background color
           ListView(
             padding: EdgeInsets.zero,
             children: <Widget>[
@@ -59,10 +56,10 @@ class MyDrawer extends StatelessWidget {
   }
 
   Widget _buildNeumorphicButton(
-      BuildContext context, {
-        required String buttonText,
-        required VoidCallback onPressed,
-      }) {
+    BuildContext context, {
+    required String buttonText,
+    required VoidCallback onPressed,
+  }) {
     return Center(
       child: Neumorphic(
         style: neumorphicButtonStyle(context, isSelected: false),
@@ -70,7 +67,7 @@ class MyDrawer extends StatelessWidget {
           onTap: onPressed,
           child: Container(
             width: MediaQuery.of(context).size.width * 0.7,
-            padding: EdgeInsets.symmetric(vertical: 16),
+            padding: EdgeInsets.symmetric(vertical: 12),
             child: Center(
               child: Text(
                 buttonText,
